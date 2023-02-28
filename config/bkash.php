3b78969c64b9f195f5293fe9e49136be44023cf3
Debdulal Baidya <ovibaidya72@gmail.com>
2023-02-28 13:13:12 +0600

    bkash config file included

---
+++ config/bkash.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Bkash Configuration File
+    |--------------------------------------------------------------------------
+    |
+    | This file contains settings and parameters that are used to configure the Bkash payment gateway. 
+    | This file typically includes information such as API keys, merchant account details, 
+    | encryption keys, and other settings required to enable Bkash payments for a website or application. 
+    | It is important to configure this file correctly in order to ensure that Bkash payments are 
+    | processed securely and efficiently.
+    |
+    */
+    
+];
